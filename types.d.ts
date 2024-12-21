458a13d6f2c227d6ec854bb8a8d6f56342b263ca
Maurycy Borkowski <maurycy.borkowski@gmail.com>
2024-12-21 01:08:58 +0100

    fix libs

--- types.d.ts
+++ types.d.ts
@@ -1,9 +1,13 @@
+import { Object3DNode } from '@react-three/fiber';
 import * as THREE from 'three';
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      axesHelper: ReactThreeFiber.Object3DNode<THREE.AxesHelper, typeof THREE.AxesHelper>;
+      ambientLight: Object3DNode<THREE.AmbientLight, typeof THREE.AmbientLight>;
+      directionalLight: Object3DNode<THREE.DirectionalLight, typeof THREE.DirectionalLight>;
+      gridHelper: Object3DNode<THREE.GridHelper, typeof THREE.GridHelper>;
+      axesHelper: Object3DNode<THREE.AxesHelper, typeof THREE.AxesHelper>;
     }
   }
 }
